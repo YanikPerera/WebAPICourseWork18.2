9813c75dc513689475a4d4764d211f65ac9a477b
anjalee-abenayaka <anjalee-abenayaka@users.noreply.github.com>
2020-03-06 20:40:57 +0530

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Component} from "react";
+class Home extends Component{
+    state= {};
+    render(){
+        return(
+        <h1>Welcome to Home Page</h1>
+          );
+    }
+}
+export default Home;
