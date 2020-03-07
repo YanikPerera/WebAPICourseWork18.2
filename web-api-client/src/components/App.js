b7841d1beb983ba6e2a4f2463af123e1647895b3
anjalee-abenayaka <anjalee-abenayaka@users.noreply.github.com>
2020-03-07 22:40:10 +0530

    added SignUp UI

--- web-api-client/src/components/App.js
+++ web-api-client/src/components/App.js
@@ -6,6 +6,7 @@ import LandingPage from "./views/LandingPage/LandingPage";
 import NavBar from "./views/NavBar/NavBar";
 import Auth from "../hoc/auth";
 import LoginPage from "./views/LoginPage/LoginPage";
+import RegisterPage from "./views/RegisterPage/RegisterPage";
 function App() {
     return (
       <Suspense fallback={(<div>Loading...</div>)}>
@@ -14,6 +15,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Auth(LandingPage, null)} />
             <Route exact path="/login" component={Auth(LoginPage, false)} />
+            <Route exact path="/register" component={Auth(RegisterPage, false)} />
           </Switch>
         </div>
         <Footer />
